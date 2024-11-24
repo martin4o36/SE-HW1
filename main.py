011987ab2de3a354c0bed195e598c8680d674dce
Martin Kozarev <marti.kozarev@gmail.com>
2024-11-24 17:34:32 +0200

    Everything is right now

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from shapes import *
 
 def main():
-    while True:
-        try:
-            width = 2.5
-            height = 10
-            print(f"Area of rectangle: {area_of_rectangle(width, height)}")
+    try:
+        width = 2.5
+        height = 10
+        print(f"Area of rectangle: {area_of_rectangle(width, height)}")
 
-            radius = 7
-            print(f"Area of circle: {area_of_circle(radius)}")
+        radius = 7
+        print(f"Area of circle: {area_of_circle(radius)}")
 
-            base = 2.335
-            triangle_height = 2
-            print(f"Area of triangle: {area_of_triangle(base, triangle_height)}")
+        base = 2.335
+        triangle_height = 2
+        print(f"Area of triangle: {area_of_triangle(base, triangle_height)}")
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
